95398ab0784fdb03100f59124ca1e34889e984e2
Attila Szakacs <attila.szakacs@balabit.com>
2019-10-29 12:41:53 +0100

    config_helper 2
    Signed-off-by: Attila Szakacs <attila.szakacs@balabit.com>

--- config-option-database/config_helper.py
+++ config-option-database/config_helper.py
@@ -24,97 +24,55 @@
 from tempfile import NamedTemporaryFile
 
 from utils.MergeYm import merge_grammars
-from utils.ConfigGraph import BisonGraph, SyntaxGraph, cut_at_types, remove_code_blocks, OptionParser
-from argparse import ArgumentParser, REMAINDER
+from utils.ConfigGraph import BisonGraph, cut_at_types, remove_code_blocks, OptionParser
+from argparse import ArgumentParser
 
-contexts = [
-    'LL_CONTEXT_SOURCE',
-    'LL_CONTEXT_DESTINATION'
-]
-
-def get_options_dict(graph):
-    all_options = set()
-    paths = filter(lambda x: x[0] in contexts, graph.get_paths())
-    for path in paths:
-        options = OptionParser(path).get_options()
-        for option in options:
-            all_options.add(option)
-    options_dict = {}
-    for context in contexts:
-        options_dict[context] = {}
-    for option in all_options:
-        context = option.drivers[0][0]
-        driver = option.drivers[0][1]
-        if driver not in options_dict[context].keys():
-            options_dict[context][driver] = []
-        entry = {}
-        entry['keyword'] = option.keyword
-        entry['types'] = option.types
-        if option.parents:
-            parent = option.parents[0]
-            if parent not in options_dict[context][driver].keys():
-                options_dict[context][driver][parent] = []
-            options_dict[context][driver][parent].append(entry)
-        else:
-            options_dict[context][driver].append(entry)
-    return options_dict
-
-def get_graph():
+def get_paths():
     with NamedTemporaryFile(mode='w+') as yaccfile:
         merge_grammars(yaccfile.name)
         graph = BisonGraph(yaccfile.name)
     cut_at_types(graph)
     remove_code_blocks(graph)
-    return graph
+    return graph.get_paths()
 
-def deep_print(block, depth=1):
-    options = list(block['options'])
-    options.sort()
-    for option in options:
-        keyword = option[0]
-        types = option[1]
-        if keyword:
-            print('  '*depth+keyword+': '+'<'+types+'>')
-        else:
-            print('  '*depth+'<'+types+'>')
-    for key in block['blocks'].keys():
-        print('  '*depth+key+':')
-        deep_print(block['blocks'][key], depth+1)
+def get_db():
+    contexts = ['LL_CONTEXT_SOURCE', 'LL_CONTEXT_DESTINATION']
+    paths = filter(lambda x: x[0] in contexts, get_paths())
+    db = dict.fromkeys(contexts, {})
+    for path in paths:
+        for option in OptionParser(path).get_options():
+            context, driver = option.drivers[0]
+            keyword = option.keyword
+            db[context].setdefault(driver, {'options': set(), 'blocks': {}})
+            add_to = db[context][driver]
+            for parent in option.parents:
+                add_to['blocks'].setdefault(parent, {'options':set(), 'blocks':{}})
+                add_to = add_to['blocks'][parent]
+            add_to['options'].add((keyword if keyword else '', ' '.join(option.types)))
+    return db
 
+def print_options(db, context, driver):
+    print('{} {}:'.format(context, driver))
+    print_options_helper(db[context][driver], 1)
 
-def main():
+def print_options_helper(block, depth):
+    indent = '  ' * depth
+    for keyword, arguments in sorted(block['options']):
+        print('{}{}<{}>'.format(indent, (keyword + ': ') if keyword else '', arguments))
+    for key in sorted(block['blocks'].keys()):
+        print('{}{}:'.format(indent, key))
+        print_options_helper(block['blocks'][key], depth + 1)
+
+def parse_args():
     parser = ArgumentParser()
     parser.add_argument('context', type=str, help='source/destination')
     parser.add_argument('driver', type=str, help='driver')
     args = parser.parse_args()
+    return args.context, args.driver
 
-    graph = get_graph()
-    paths = filter(lambda x: x[0] in contexts, graph.get_paths())
-    db = {}
-    for context in contexts:
-        db[context] = {}
-    for path in paths:
-        for option in OptionParser(path).get_options():
-            context = option.drivers[0][0]
-            driver = option.drivers[0][1]
-            if not driver in db[context].keys():
-                db[context][driver] = {'options':set(), 'blocks':{}}
-            insert_to = db[context][driver]
-            for parent in option.parents:
-                if not parent in insert_to['blocks'].keys():
-                    insert_to['blocks'][parent] = {'options':set(), 'blocks':{}}
-                insert_to = insert_to['blocks'][parent]
-            keyword = option.keyword
-            if keyword == None:
-                keyword = ''
-            insert_to['options'].add((keyword, ' '.join(option.types)))
-    # import json
-    # print(json.dumps(db, indent=2))
-
-    print(args.context+' '+args.driver+':')
-    deep_print(db[args.context][args.driver])
-
-
+def main():
+    context, driver = parse_args()
+    print_options(get_db(), context, driver)
 
 if __name__ == '__main__':
     main()
