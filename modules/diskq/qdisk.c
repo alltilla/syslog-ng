c025d713a5210180c64198d58543bfcc45f161f3
Attila Szakacs <attila.szakacs@oneidentity.com>
2021-07-22 09:22:05 +0200

    qdisk: factor out _update_positions_after_read()
    Signed-off-by: Attila Szakacs <attila.szakacs@oneidentity.com>

--- modules/diskq/qdisk.c
+++ modules/diskq/qdisk.c
@@ -525,19 +525,9 @@ _calculate_new_read_head_position(QDisk *self, guint32 record_length)
   return new_read_head_position;
 }
 
-gboolean
-qdisk_pop_head(QDisk *self, GString *record)
+static void
+_update_positions_after_read(QDisk *self, guint32 record_length)
 {
-  if (self->hdr->read_head == self->hdr->write_head)
-    return FALSE;
-
-  guint32 record_length;
-  if (!_try_reading_record_length(self, &record_length))
-    return FALSE;
-
-  if (!_read_record_from_disk(self, record, record_length))
-    return FALSE;
-
   self->hdr->read_head = _calculate_new_read_head_position(self, record_length);
   self->hdr->length--;
 
@@ -551,6 +541,23 @@ qdisk_pop_head(QDisk *self, GString *record)
           qdisk_reset_file_if_empty(self);
         }
     }
+}
+
+gboolean
+qdisk_pop_head(QDisk *self, GString *record)
+{
+  if (_is_trying_to_read_from_unwritten_space(self))
+    return FALSE;
+
+  guint32 record_length;
+  if (!_try_reading_record_length(self, &record_length))
+    return FALSE;
+
+  if (!_read_record_from_disk(self, record, record_length))
+    return FALSE;
+
+  _update_positions_after_read(self, record_length);
+
   return TRUE;
 }
 
