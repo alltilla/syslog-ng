a9ed99a0099614ab6cc9a6ae3190fc231edc4d61
Balázs Barkó <Balazs.Barko@oneidentity.com>
2021-08-18 15:07:05 +0200

    stats: create timer to aggregated stats
    Signed-off-by: Balázs Barkó <Balazs.Barko@oneidentity.com>

--- lib/stats/stats-aggregator-register.c
+++ lib/stats/stats-aggregator-register.c
@@ -25,7 +25,10 @@
 #include "stats/stats-registry.h"
 #include "stats/stats-query.h"
 #include "cfg.h"
+#include "iv.h"
+#include "timeutils/cache.h"
 
+#define FREQUENCY_OF_UPDATE 60
 
 typedef struct _StatsClusterContainer
 {
@@ -36,7 +39,53 @@ static StatsAggregatorClusterContainer stats_cluster_container;
 
 static GStaticMutex stats_aggregator_mutex = G_STATIC_MUTEX_INIT;
 static gboolean stats_aggregator_locked;
+static struct iv_timer update_timer;
 
+static void
+_update_func (gpointer _key, gpointer _value, gpointer _user_data)
+{
+  StatsAggregator *self = (StatsAggregator *) _value;
+  stats_aggregator_aggregate(self);
+}
+
+static void
+_start_timer(void)
+{
+  iv_validate_now();
+  update_timer.expires = iv_now;
+  update_timer.expires.tv_sec += FREQUENCY_OF_UPDATE;
+
+  if (!iv_timer_registered(&update_timer))
+    iv_timer_register(&update_timer);
+}
+
+static void
+_update(void *cookie)
+{
+  g_hash_table_foreach(stats_cluster_container.clusters, _update_func, NULL);
+}
+
+static void
+_init_timer(void)
+{
+  IV_TIMER_INIT(&update_timer);
+  update_timer.cookie = NULL;
+  update_timer.handler = _update;
+  _start_timer();
+}
+
+static void
+_stop_timer(void)
+{
+  if (iv_timer_registered(&update_timer))
+    iv_timer_unregister(&update_timer);
+}
+
+static void
+_deinit_timer(void)
+{
+  _stop_timer();
+}
 
 void
 stats_aggregator_lock(void)
@@ -73,6 +122,7 @@ stats_aggregator_registry_deinit(void)
   g_hash_table_destroy(stats_cluster_container.clusters);
   stats_cluster_container.clusters = NULL;
   g_static_mutex_free(&stats_aggregator_mutex);
+  _deinit_timer();
 }
 
 static void
