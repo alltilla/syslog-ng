451ad22048aa000a94761f75a861c15e06a3bfa6
Attila Szakacs <szakacs.attila96@gmail.com>
2023-03-04 16:43:45 +0100

    python-options: add PythonOption skeleton
    Signed-off-by: Attila Szakacs <szakacs.attila96@gmail.com>

---
+++ modules/python/python-options.c
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2023 Attila Szakacs
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published
+ * by the Free Software Foundation, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As an additional exemption you are allowed to compile & link against the
+ * OpenSSL libraries as published by the OpenSSL project. See the file
+ * COPYING for details.
+ *
+ */
+
+#include "python-options.h"
+#include "str-utils.h"
+
+/* Python Option */
+
+struct _PythonOption
+{
+  gchar *name;
+
+  PyObject *(*create_value_py_object)(const PythonOption *s);
+  PythonOption *(*clone)(const PythonOption *s);
+  void (*free_fn)(PythonOption *s);
+};
+
+static void
+python_option_init_instance(PythonOption *s, const gchar *name)
+{
+  s->name = __normalize_key(name);
+}
+
+const gchar *
+python_option_get_name(const PythonOption *s)
+{
+  return s->name;
+}
+
+PyObject *
+python_option_create_value_py_object(const PythonOption *s)
+{
+  g_assert(s->create_value_py_object);
+
+  PyGILState_STATE gstate = PyGILState_Ensure();
+  PyObject *value = s->create_value_py_object(s);
+  PyGILState_Release(gstate);
+
+  return value;
+}
+
+PythonOption *
+python_option_clone(const PythonOption *s)
+{
+  g_assert(s->clone);
+  return s->clone(s);
+}
+
+void
+python_option_free(PythonOption *s)
+{
+  if (s->free_fn)
+    s->free_fn(s);
+
+  g_free(s->name);
+  g_free(s);
+}
